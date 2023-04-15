f0d5cc21c9356a7db4909281e72c1a94debfd811
Araya <aray2891@mail.ru>
2023-04-15 15:11:58 +0600

    feat: solve transform-array

--- src/testt.js
+++ src/testt.js
@@ -1,79 +1,40 @@
-const fakeDate = {
-  toString() {
-    return Date.prototype.toString.call(new Date());
-  },
-  [Symbol.toStringTag]: "Date",
-};
-
-const deeperFakeDate = {
-  toString() {
-    return Date.prototype.toString.call(new Date());
-  },
-  getMonth() {
-    return Date.prototype.getMonth.call(new Date());
-  },
-  getFullYear() {
-    return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
-  },
-  getDate() {
-    return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
-  },
-  getHours() {
-    return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
-  },
-  getMinutes() {
-    return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
-  },
-  getSeconds() {
-    return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
-  },
-  getMilliseconds() {
-    return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
-  },
-  getDay() {
-    return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
-  },
-  [Symbol.toStringTag]: "Date",
-};
-
-function getSeason(date) {
-  // if (isNaN(date)) {
-  //   return "Unable to determine the time of year!";
-  // }
-  if (date === undefined) {
-    return "Unable to determine the time of year!";
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("'arr' parameter must be an instance of the Array!");
   }
-  if (!(date instanceof Date || !isNaN(date))) {
-    throw new Error("Invalid date!");
+  let arr2 = Object.values(arr);
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === "--discard-next") {
+      delete arr2[i + 1];
+      arr2.splice(i, 1);
+    }
+    if (arr2[i] === "--discard-prev" && arr2[i - 1]) {
+      delete arr2[i - 1];
+      arr2.splice(i, 1);
+    } else if (arr2[i] === "--discard-prev" && !arr2[i - 1]) {
+      arr2.splice(i, 1);
+    }
+    if (arr2[i] === "--double-next" && arr2[i + 1]) {
+      arr2.splice(i, 1, arr2[i + 1]);
+    } else if (arr2[i] === "--double-next" && !arr2[i + 1]) {
+      arr2.splice(i, 1);
+    }
+    if (arr2[i] === "--double-prev" && arr2[i - 1]) {
+      arr2.splice(i, 1, arr2[i - 1]);
+    } else if (arr2[i] === "--double-prev" && !arr2[i - 1]) {
+      arr2.splice(i, 1);
+    }
   }
 
-  if (
-    date.getMonth() === 11 ||
-    date.getMonth() === 0 ||
-    date.getMonth() === 1
-  ) {
-    return "winter";
-  } else if (
-    date.getMonth() === 2 ||
-    date.getMonth() === 3 ||
-    date.getMonth() === 4
-  ) {
-    return "spring";
-  } else if (
-    date.getMonth() === 5 ||
-    date.getMonth() === 6 ||
-    date.getMonth() === 7
-  ) {
-    return "summer";
-  } else if (
-    date.getMonth() === 8 ||
-    date.getMonth() === 9 ||
-    date.getMonth() === 10
-  ) {
-    return "autumn";
-  } else {
-    throw new Error("Invalid date!");
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === undefined) {
+      arr2.splice(i, 1);
+    }
   }
+  return arr2;
 }
 
-console.log(getSeason(() => new Date(deeperFakeDate)));
+console.log(
+  transform([1, 2, 3, "--double-next", 1337, "--discard-prev", 4, 5])
+);
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,55 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("'arr' parameter must be an instance of the Array!");
+  }
+  let arr2 = Object.values(arr);
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === "--discard-next") {
+      delete arr2[i + 1];
+      arr2.splice(i, 1);
+    }
+    if (arr2[i] === "--discard-prev" && arr2[i - 1]) {
+      delete arr2[i - 1];
+      arr2.splice(i, 1);
+    } else if (arr2[i] === "--discard-prev" && !arr2[i - 1]) {
+      arr2.splice(i, 1);
+    }
+    if (arr2[i] === "--double-next" && arr2[i + 1]) {
+      arr2.splice(i, 1, arr2[i + 1]);
+    } else if (arr2[i] === "--double-next" && !arr2[i + 1]) {
+      arr2.splice(i, 1);
+    }
+    if (arr2[i] === "--double-prev" && arr2[i - 1]) {
+      arr2.splice(i, 1, arr2[i - 1]);
+    } else if (arr2[i] === "--double-prev" && !arr2[i - 1]) {
+      arr2.splice(i, 1);
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === undefined) {
+      arr2.splice(i, 1);
+    }
+  }
+  return arr2;
 }
 
 module.exports = {
-  transform
+  transform,
 };
